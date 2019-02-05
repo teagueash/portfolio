86720e9f61bef53322e7066657b0ac0fa8149391
Teague  Ashcraft <tkashcra@usc.edu>
2019-02-05 14:10:38 -0800

    Updates

---
+++ precache-manifest.a789cf06c56b16472951b8621c6eeb26.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f0742e2728d95a2a7111aa3b2f02b94",
+    "url": "/portfolio/static/media/expeditions.4f0742e2.png"
+  },
+  {
+    "revision": "7b5fa76e75d0eaa28f37",
+    "url": "/portfolio/static/css/main.5fe875d7.chunk.css"
+  },
+  {
+    "revision": "a6373c2347c95857ec26",
+    "url": "/portfolio/static/js/1.a6373c23.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "f97cb68b23122139107125eba967e662",
+    "url": "/portfolio/static/media/coordinator.f97cb68b.jpg"
+  },
+  {
+    "revision": "e9d81a7cbfe9b05ab14749bf34b98ee5",
+    "url": "/portfolio/static/media/tragedy.e9d81a7c.jpg"
+  },
+  {
+    "revision": "7b5fa76e75d0eaa28f37",
+    "url": "/portfolio/static/js/main.7b5fa76e.chunk.js"
+  },
+  {
+    "revision": "f3f5e666f344009ea1ea1f1e22a4e8f4",
+    "url": "/portfolio/static/media/ufoviz.f3f5e666.jpg"
+  },
+  {
+    "revision": "c2e5f03f276f36636e2b13f4b8fbda1d",
+    "url": "/portfolio/static/media/vscroll.c2e5f03f.png"
+  },
+  {
+    "revision": "c7f103f017202d7865dc70b43f399617",
+    "url": "/portfolio/static/media/zookeeper.c7f103f0.png"
+  },
+  {
+    "revision": "4e50f8e5b27cc3c04ce6c1e956bf3055",
+    "url": "/portfolio/static/media/fruitrage.4e50f8e5.png"
+  },
+  {
+    "revision": "42b253e73aa525ed91c9b46fdefe6072",
+    "url": "/portfolio/static/media/inference.42b253e7.png"
+  },
+  {
+    "revision": "9d2b892baf12b93efb1b526f725b1d2c",
+    "url": "/portfolio/index.html"
+  }
+];
