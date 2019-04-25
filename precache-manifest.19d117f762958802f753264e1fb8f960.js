9e218781611068375a663977af28eed8ecf9dcd7
Teague  Ashcraft <tkashcra@usc.edu>
2019-04-25 10:11:16 -0700

    Updates

--- precache-manifest.19d117f762958802f753264e1fb8f960.js
+++ precache-manifest.19d117f762958802f753264e1fb8f960.js
@@ -1,15 +1,15 @@
 self.__precacheManifest = [
   {
-    "revision": "4f0742e2728d95a2a7111aa3b2f02b94",
-    "url": "/portfolio/static/media/expeditions.4f0742e2.png"
+    "revision": "f3f5e666f344009ea1ea1f1e22a4e8f4",
+    "url": "/portfolio/static/media/ufoviz.f3f5e666.jpg"
   },
   {
-    "revision": "d6786840dd1d5f424e68",
-    "url": "/portfolio/static/css/main.ab475bc6.chunk.css"
+    "revision": "82f939df25cd651bb8ab",
+    "url": "/portfolio/static/css/main.4be6911f.chunk.css"
   },
   {
-    "revision": "899e72aaff02e934b39d",
-    "url": "/portfolio/static/js/1.899e72aa.chunk.js"
+    "revision": "04898cade18213ccb7a3",
+    "url": "/portfolio/static/js/1.04898cad.chunk.js"
   },
   {
     "revision": "ee2a100325fd284f0097",
@@ -24,16 +24,20 @@ self.__precacheManifest = [
     "url": "/portfolio/static/media/tragedy.e9d81a7c.jpg"
   },
   {
-    "revision": "d6786840dd1d5f424e68",
-    "url": "/portfolio/static/js/main.d6786840.chunk.js"
+    "revision": "4f0742e2728d95a2a7111aa3b2f02b94",
+    "url": "/portfolio/static/media/expeditions.4f0742e2.png"
   },
   {
-    "revision": "f3f5e666f344009ea1ea1f1e22a4e8f4",
-    "url": "/portfolio/static/media/ufoviz.f3f5e666.jpg"
+    "revision": "82f939df25cd651bb8ab",
+    "url": "/portfolio/static/js/main.82f939df.chunk.js"
   },
   {
     "revision": "c2e5f03f276f36636e2b13f4b8fbda1d",
     "url": "/portfolio/static/media/vscroll.c2e5f03f.png"
+  },
+  {
+    "revision": "9c5533119441260709b56086ae597008",
+    "url": "/portfolio/static/media/observer-infinite-scroll.9c553311.png"
   },
   {
     "revision": "c7f103f017202d7865dc70b43f399617",
@@ -48,7 +52,7 @@ self.__precacheManifest = [
     "url": "/portfolio/static/media/inference.42b253e7.png"
   },
   {
-    "revision": "29a0bd36f43d5b995bab23c62e178a96",
+    "revision": "708ad8e15d27699993ba8aa06da0f47a",
     "url": "/portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.196fa64e936f9f58428bb6713e851c6d.js"
+  "/portfolio/precache-manifest.19d117f762958802f753264e1fb8f960.js"
 );
 
 workbox.clientsClaim();
